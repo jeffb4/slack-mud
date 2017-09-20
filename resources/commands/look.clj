f62e748cb682aa109ac021fe11cef1f10f0cda85
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2017-09-20 11:46:01 -0400

    Send message back to player

--- resources/commands/look.clj
+++ resources/commands/look.clj
@@ -1,5 +1,17 @@
 { :aliases ["look" "l"]
   :fn (fn [message user]
         (do
-          (println (str "in look with message" (pr-str message)))
-          (eval (read-string message))))}
+          (clojure.tools.logging/info "In look with message:" [message user])
+          (try
+            ((juxt
+              #(slack-mud.message-handler/send_message
+                  (str "Short: "
+                    (:shortdesc %1))
+                  user)
+              #(slack-mud.message-handler/send_message
+                  (str "Desc: "
+                    (:desc %1))
+                  user))
+
+             (get @slack-mud.rooms/rooms (:location user)))
+            (catch RuntimeException e (prn "Runtime exception:")))))}
