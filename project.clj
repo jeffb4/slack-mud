def16199d9dc4a70f564b001036117c1f65dfb6a
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2017-09-20 11:45:39 -0400

    Add logging lib

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.scala-lang/scala-library "2.12.3"]
+                 [org.clojure/tools.logging "0.4.0"]
                  [org.julienxx/clj-slack "0.5.5"]
                  [aleph "0.4.3"]
-                 [manifold "0.1.6"]
                  [cheshire "5.8.0"]
-                 [io.forward/yaml "1.0.6"]]
-
+                 [io.forward/yaml "1.0.6"]
+                 [manifold "0.1.6"]]
+  :source-paths ["src"]
   :main ^:skip-aot slack-mud.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
