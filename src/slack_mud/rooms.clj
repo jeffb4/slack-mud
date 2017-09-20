bd2535239ff47dbc18ca9f980fddf67a18143a9a
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2017-09-20 11:57:05 -0400

    refactor room handling

--- src/slack_mud/rooms.clj
+++ src/slack_mud/rooms.clj
@@ -1,23 +1,27 @@
 (ns slack-mud.rooms)
 
+(require
+  '[clojure.string :as str])
+
 (def rooms (ref {}))
 
-(defn load-room 
+(defn load-room
   "Load a room file, returns updated room map"
   [rooms file]
   (let [room (read-string (slurp (.getAbsolutePath file)))]
     (conj rooms
-          {(keyword (.getName file))
+          {(str/replace (.getName file) #"\.clj" "")
            {:name (keyword (.getName file))
             :desc (:desc room)
-            :exits (ref (:exits room))
+            :shortdesc (:shortdesc room)
+            :exits (ref (or (:exits room) #{}))
             :items (ref (or (:items room) #{}))
             :inhabitants (ref #{})}})))
 
 (defn load-rooms
   "Given a dir, return a map with an entry corresponding to each file
   in it. Files should be maps containing room data."
-  [rooms dir]
+  [dir]
   (dosync
-    (reduce load-room rooms
-           (.listFiles (java.io.File. dir)))))
+    (alter rooms conj (reduce load-room @rooms
+                       (.listFiles (java.io.File. dir))))))
